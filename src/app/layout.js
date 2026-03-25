8cc6e27f58dd5a2cfa1c5de9046d9b1bd9a6a4ef
fede bustamante <federicobustamantevyp@gmail.com>
2026-03-25 19:45:19 -0300

    cambios icono

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/loadingg.png" type="image/png" />
+        <link rel="icon" href="/loading.ico" type="image/png" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
